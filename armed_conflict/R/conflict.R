678d13745a589ab42f6ea6ac505a110e5ebad17b
gmasuncion <gm.asuncion@hotmail.com>
2024-09-29 19:48:26 -0400

    Readding total number of conflicts as a variable

--- armed_conflict/R/conflict.R
+++ armed_conflict/R/conflict.R
@@ -6,6 +6,6 @@ dat <- read.csv("armed_conflict/original/conflictdata.csv")
 
 # Deriving the binary armed conflict variable
 dat <- dat %>% group_by(year,ISO) %>% summarize(conflict = sum(best))
-dat$conflict <- ifelse(dat$conflict >= 25, 1, 0)
+dat$armedconf <- ifelse(dat$conflict >= 25, 1, 0)
 dat$year <- dat$year + 1
 conflict_final <- dat
